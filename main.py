52e9287d9545c930ab38f7943dfe321cfa6e8208
Asdergh <ramzanhac2005@gmail.com>
2023-06-07 23:44:03 +0300

    all changes were added

--- main.py
+++ main.py
@@ -1,65 +1,85 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as manimation
-
-plt.style.use("seaborn")
-figure = plt.figure()
-surface = figure.add_subplot(projection="3d")
-surface.set_xlabel("cores_label_x")
-surface.set_ylabel("cores_label_y")
-surface.set_zlabel("cores_label_z")
-
-def trajectory(data_type):
-    datacores = pd.read_json("data.csv")
-    if data_type != "grid":
-        x_cores, y_cores, z_cores = [], [], []
-        def trajectory_visualisation(time):
-            surface.clear()
-            x_cores.append(datacores.iloc[time, [1]])
-            y_cores.append(datacores.iloc[time, [2]])
-            z_cores.append(datacores.iloc[time, [3]])
-            surface.plot(x_cores, y_cores, z_cores, color="red")
-        anim = manimation.FuncAnimation(figure, trajectory_visualisation, interval=99)
-
-    else:
-        x_cores = np.asarray(datacores.iloc[:, 0:100])
-        y_cores = np.asarray(datacores.iloc[:, 100:200])
-        z_cores = np.asarray(datacores.iloc[:, 200:300])
-        i, j, k = [], [], []
-        def surface_simulation(time):
-            surface.clear()
-            i = x_cores[:, 0:time]
-            j = y_cores[:, 0:time]
-            k = z_cores[:, 0:time]
-            surface.plot_surface(i, j, k, alpha=0.7, cmap="coolwarm")
-            surface.scatter(i, j, k, c=k, cmap="binary", s=0.10)
-        anim = manimation.FuncAnimation(figure, surface_simulation, interval=100)
-
-    plt.show()
-
-def create_new_datacores(x, y, z, file_name="data.csv"):
-    try:
-        cores = pd.DataFrame(np.array([x, y, z]).T, columns=["x: ", "y: ", "z: "])
-        cores.to_csv(file_name)
-    except ValueError:
-        print("[grid refactoring]!!!")
-        cores = pd.DataFrame(np.hstack((x, y, z)))
-        cores.to_json(file_name)
+import math as mt
+import random as rd
 
 
+
+
+class DATA_Vis():
+
+    def __init__(self, file_name) -> None:
+        self.file_name = file_name
+        self.figure = plt.figure()
+        self.plane = self.figure.add_subplot(projection="3d")
     
-
-theta, psi = np.meshgrid(np.linspace(-np.pi, np.pi, 100),
-                         np.linspace(-np.pi, np.pi, 100))
-
-x_cores, y_cores = np.meshgrid(np.linspace(-np.pi, np.pi, 100),
-                               np.linspace(-np.pi, np.pi, 100))
-z_cores = np.sin(np.sqrt(x_cores ** 2 +  y_cores ** 2) + np.exp(x_cores ** 2 + y_cores ** 2)) / np.sqrt(x_cores ** 2 + y_cores ** 2)
-
-create_new_datacores(x_cores, y_cores, z_cores)
-#surface.plot_surface(x_cores, y_cores, z_cores)
-#plt.show()
-trajectory(data_type="grid")
-
+    def load_data(self):
+        self.grid_flag = False
+        data = pd.read_json(self.file_name)
+        if len(data.to_numpy()[0]) == 3:
+            self.x_cores = np.asarray(data.iloc[:, 0])
+            self.y_cores = np.asarray(data.iloc[:, 1])
+            self.z_cores = np.asarray(data.iloc[:, 2])
+        else:
+            self.x_cores = np.asarray(data.iloc[:, 0:100])
+            self.y_cores = np.asarray(data.iloc[:, 100:200])
+            self.z_cores = np.asarray(data.iloc[:, 200:300])
+            self.grid_flag = True
     
+    def plot_graph_animaiton(self):
+        print(self.y_cores)
+        def graph_surface(time):
+            self.plane.clear()
+            self.plane.scatter(self.x_cores[:, 0:time], self.y_cores[:, 0:time], self.z_cores[:, 0:time], c=self.z_cores[:, 0:time], cmap="viridis", s=0.089)
+            self.plane.plot_surface(self.x_cores[:, 0:time], self.y_cores[:, 0:time], self.z_cores[:,0:time], alpha=0.5, cmap="bone")
+        
+        def graph_trajectory(time):
+            self.plane.clear()
+            self.plane.plot(self.x_cores[0:time], self.y_cores[0:time], self.z_cores[0:time], alpha=0.5, color="blue", linestyle="--")
+            self.plane.scatter(self.x_cores[time], self.y_cores[time], self.z_cores[time], alpha=0.2, color="red")
+            self.plane.quiver(self.x_cores[time], self.y_cores[time], self.z_cores[time], self.x_cores[time + 1], 0, 0, color="red")
+            self.plane.quiver(self.x_cores[time], self.y_cores[time], self.z_cores[time], 0, self.y_cores[time + 1], 0, color="blue")
+            self.plane.quiver(self.x_cores[time], self.y_cores[time], self.z_cores[time], 0, 0, self.z_cores[time + 1], color="green")
+
+        
+        if self.grid_flag == True:
+            animo = manimation.FuncAnimation(self.figure, graph_surface, interval=100)
+            plt.show()
+        else:
+            animo = manimation.FuncAnimation(self.figure, graph_trajectory, interval=100)
+            plt.show()
+
+
+data = DATA_Vis(file_name="trajectory.json")
+data.load_data()
+data.plot_graph_animaiton()
+
+def lorenc(xyz, s=10, r=28, b=2.668):
+    x, y, z = xyz
+    x_curent = s * (y - x)
+    y_curent = r * x - y - x * z
+    z_curent = x * y - b * z
+    
+    return np.array([x_curent, y_curent, z_curent])
+
+dt = 0.01
+xyz = np.empty((10000 + 1, 3))
+xyz[0] = (0., 1., 1.05)
+for iter in range(10000):
+    xyz[iter + 1] = xyz[iter] + lorenc(xyz[iter]) * dt
+#xyz = xyz.T
+
+cores = pd.DataFrame(xyz)
+print(cores.to_numpy().shape)
+cores.to_json("trajectory.json")
+
+
+
+
+        
+        
+
+
+
---
+++ test_1.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as manimation
+
+plt.style.use("seaborn")
+figure = plt.figure()
+surface = figure.add_subplot(projection="3d")
+surface.set_xlabel("cores_label_x")
+surface.set_ylabel("cores_label_y")
+surface.set_zlabel("cores_label_z")
+
+def trajectory(data_type):
+    datacores = pd.read_json("data.csv")
+    if data_type != "grid":
+        x_cores, y_cores, z_cores = [], [], []
+        def trajectory_visualisation(time):
+            surface.clear()
+            x_cores.append(datacores.iloc[time, [1]])
+            y_cores.append(datacores.iloc[time, [2]])
+            z_cores.append(datacores.iloc[time, [3]])
+            surface.plot(x_cores, y_cores, z_cores, color="red")
+        anim = manimation.FuncAnimation(figure, trajectory_visualisation, interval=100)
+
+    else:
+        x_cores = np.asarray(datacores.iloc[:, 0:100])
+        y_cores = np.asarray(datacores.iloc[:, 100:200])
+        z_cores = np.asarray(datacores.iloc[:, 200:300])
+        i, j, k = [], [], []
+        def surface_simulation(time):
+            surface.clear()
+            i = x_cores[:, 0:time]
+            j = y_cores[:, 0:time]
+            k = z_cores[:, 0:time]
+            surface.plot_surface(i, j, k, alpha=0.3, cmap="bone")
+            surface.scatter(i, j, k, c=k, cmap="magma", s=0.10)
+        anim = manimation.FuncAnimation(figure, surface_simulation, interval=100)
+
+    plt.show()
+
+def create_new_datacores(x, y, z, file_name="data.csv"):
+    try:
+        cores = pd.DataFrame(np.array([x, y, z]).T, columns=["x: ", "y: ", "z: "])
+        cores.to_csv(file_name)
+    except ValueError:
+        print("[grid refactoring]!!!")
+        cores = pd.DataFrame(np.hstack((x, y, z)))
+        cores.to_json(file_name)
+
+
+    
+
+theta, psi = np.meshgrid(np.linspace(-np.pi, np.pi, 100),
+                         np.linspace(-np.pi, np.pi, 100))
+
+x_cores, y_cores = np.meshgrid(np.linspace(-np.pi, np.pi, 100),
+                               np.linspace(-np.pi, np.pi, 100))
+z_cores = np.sin(np.sqrt(x_cores ** 2 + y_cores ** 2) + np.exp(x_cores ** 2 + y_cores ** 2)) / np.sqrt(x_cores ** 2 + y_cores ** 2)
+
+create_new_datacores(x_cores, y_cores, z_cores)
+#surface.plot_surface(x_cores, y_cores, z_cores)
+#plt.show()
+trajectory(data_type="grid")
+
+    
